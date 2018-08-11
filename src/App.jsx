9f4c327e344aa1f582c5b1957653c33634371a7d
Abideen Onalaja <abideenonalaja@Abideens-MacBook-Pro.local>
2018-08-11 21:38:07 +0100

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from "./views/Home/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome</h2>
-        </div>
-        <p className="App-intro">
-          To what started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Home />
       </div>
     );
   }
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = props => (
+  <div className="home">
+    <h2>Welcome home</h2>
+  </div>
+);
+
+export default Home;
