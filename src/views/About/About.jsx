f64b1da07a3bd146afcec70880b7b0eb3e263f05
Abey Onalaja <abey.onalaja@gmail.com>
2018-08-15 22:05:21 +0100

    Create About page

---
+++ src/views/About/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { pageWithLayout } from "../../containers/page";
+
+export const About = props => {
+  <div className="about">
+    <h2>About us</h2>
+  </div>;
+};
+
+export default pageWithLayout(About);
