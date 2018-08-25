a64da228c8d5eea2b03a6b058c0272445d983dde
Abey Onalaja <abey.onalaja@gmail.com>
2018-08-25 05:11:12 +0100

    Add movie api call

--- src/data/api/utils.js
+++ src/data/api/utils.js
@@ -1,55 +1,62 @@
 import { encode } from "iconv-lite";
 
-const BASE_URL = 'localhost:3456'
-const defaultFetchOpts = {}
+const BASE_URL = "http://localhost:3456";
+const defaultFetchOpts = {};
 const defaultHeaders = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-}
+  "Content-Type": "application/json",
+  Accept: "application/json"
+};
 
-const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMDMzOTQ0ZjAtYTcyYS0xMWU4LWJmYTktYjExM2UzNjkyODFmIiwiaWF0IjoxNTM1MDY2NTA0fQ.ld5SwfP7KWF-oTVbxqLaZz9yolaNuDSz95VybSeWV-M"
+const AUTH_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMDMzOTQ0ZjAtYTcyYS0xMWU4LWJmYTktYjExM2UzNjkyODFmIiwiaWF0IjoxNTM1MDY2NTA0fQ.ld5SwfP7KWF-oTVbxqLaZz9yolaNuDSz95VybSeWV-M";
 
-const queryParams = params => Object.keys(params)
-.map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
-.join('&')
+const queryParams = params =>
+  Object.keys(params)
+    .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
+    .join("&");
 
-const completeUrl = (path, query={}) => {
-    let url = `${BASE_URL}${path}`
+const completeUrl = (path, query = {}) => {
+  let url = `${BASE_URL}${path}`;
 
-    if(Object.keys(query).length > 0){
-        url += (url.indexOf('?') === -1 ? '?' : '&')
-        + queryParams(query)
-    }
-    return url
-}
+  if (Object.keys(query).length > 0) {
+    url += (url.indexOf("?") === -1 ? "?" : "&") + queryParams(query);
+  }
+  return url;
+};
 
 const makeFetchOpts = (opts = {}) => {
-    const userHeaders = opts.headers || {}
-    delete opts.headers
-    const headers = new Headers({
-        ...defaultHeaders,
-        ...userHeaders
-    })
+  const userHeaders = opts.headers || {};
+  delete opts.headers;
+  const headers = new Headers({
+    ...defaultHeaders,
+    ...userHeaders
+  });
 
-    return {...defaultFetchOpts, ...opts, headers}
-}
+  return { ...defaultFetchOpts, ...opts, headers };
+};
 
-exports const makeFetch = (path, opts={}) => {
-    const url = completeUrl(path, opts.query)
-    const fetchOpts = makeFetchOpts(opts)
-    return fetch(url, fetchOpts)
+export const makeFetch = (path, opts = {}) => {
+  const url = completeUrl(path, opts.query);
+  const fetchOpts = makeFetchOpts(opts);
+  return fetch(url, fetchOpts)
     .then(resp => {
-        if(resp.ok) return resp.json()
-        const err = resp.statusText
-        throw new Error(err)
+      // if (resp.ok) return resp.json();
+      // const err = resp.stausText;
+      // throw new Error(err);
+      console.log(resp);
     })
-}
-exports const makeAuthFetch = (path, opts={}) => {
-    const fetchOpts = {
-        ...opts,
-        headers: {
-            'Authorization': `Bearer ${AUTH_TOKEN}`
-        }
+    .catch(error => {
+      console.log(error);
+    });
+};
+export const makeAuthFetch = (path, opts = {}) => {
+  const fetchOpts = {
+    ...opts,
+    headers: {
+      Authorization: `Bearer ${AUTH_TOKEN}`
     }
-    return makeFetch(path, fetchOpts)
-}
+  };
+
+  let a = { test: "123" };
+  return makeFetch(path, fetchOpts);
+};
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pageWithLayout } from "../../containers/page";
+import { discover } from "../../data/api/movies";
 
 export const Home = props => (
   <div className="home">
     <h2>Welcome home</h2>
+    <button onClick={() => discover()}>Discover</button>
     <Link to="/about">About</Link>
   </div>
 );
